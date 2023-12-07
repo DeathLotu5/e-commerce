ad7d56af872d40ea2429f113c45eb19db8cd5b5d
DuongPH <duongph.4a@gmail.com>
2023-12-07 15:10:02 +0700

    [Update] - [duongph]: - Adding dependency to use Eureka

--- src/main/java/com/dcrop/hightech/ecommercy/ecommercyserver/EcommercyserverApplication.java
+++ src/main/java/com/dcrop/hightech/ecommercy/ecommercyserver/EcommercyserverApplication.java
@@ -1,8 +1,11 @@
 package com.dcrop.hightech.ecommercy.ecommercyserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EcommercyserverApplication {
 
